b376ee97b4f79d715734ecf01ce40b16b9bf91e5
Miro Holopainen <mirho@eg.fi>
2022-12-01 09:11:33 +0200

    chore: finalize

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# regression-task
+# Welcome to EnerKey coding challenge!
+Regression task is a work in progress analysis for a list of facilities written with Python.
+
+## Decriptions
+This repository contains two python files: regression_task.py & data_reader.py. Repository also has three different csv files, which contain the data for analysis.
+
+regression_task.py is the file that contains the tasks to complete. If you don't have data science packages installed, there is requirements.txt file containing required packages.
+
+Your goal is to model the relationship with temperature data and watts per square. You need to manipulate various dataframes and understand linear regression a bit to succeed.
+
+## Tasks
+There are 5 tasks:
+- Task 1: Joining the temperature and consumptions
+- Task 2: Filter out saturdays and sundays
+- Task 3: Calculate W/m2
+- Task 4: Use linear regression
+- Task 5: Plot the results
+
+## Other
+You have complete freedom on how to handle the implementations. The provided functions in the regression_task, can act as a guide, but you are free to do this in any way.
+
+There is no need to create unit/integrations tests.
--- regression/regression_task.py
+++ regression/regression_task.py
@@ -3,14 +3,18 @@
 # pylint: disable=C0301
 # pylint: disable=W0613
 # pylint: disable=C0116
-
 from typing import Tuple
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
-import constants
 import data_reader
+
+
+ELECTRICITY_QUANTITY_ID = 1
+REFERENCE_TEMP = 10
+MAX_COOLING_TEMP = 30
+
 
 def join_temperature(temperatures: pd.DataFrame, consumptions: pd.DataFrame, facility_meters: pd.DataFrame) -> None:
     """
@@ -21,32 +25,40 @@ def join_temperature(temperatures: pd.DataFrame, consumptions: pd.DataFrame, fac
     return consumptions
 
 
-def filter_to_weekdays(consumptions: pd.DataFrame) -> None:
+def filter_to_weekdays(consumptions: pd.DataFrame) -> pd.DataFrame:
     """
         Task 2, filter out saturdays and sundays from the consumption data
     """
     # ...
-    return
+    return consumptions
 
 
-def add_watts_per_square(facility_consumptions_df: pd.DataFrame, facility_area: float) -> None:
+def add_watts_per_square(facility_consumptions: pd.DataFrame, facility_area: float) -> None:
     """
         Task 3, create new "column" which is W/m2
-        Hint: formula is 'consumption * 1000 / area'
+        Hint: formula is 'consumption * 1000 / facility_area'
     """
     # ...
     return
 
 
-def linear_regression(cooling_consumptions: pd.DataFrame) -> Tuple[np.array, np.array, float, float]:
+def linear_regression(facility_consumptions: pd.DataFrame) -> Tuple[np.array, np.array, float, float]:
     """
-        Task 4, do the linear regression "temperature with w/m2 ANTTI!" and return: x, y, slope and intercept
+        Task 4, model temperature relationship with Watts per square using linear regression
+        and return resulting x, y, slope and intercept. Do the regression model only those consumptions
+        that are above REFERENCE_TEMP
     """
+    # Hint: cooling_consumptions = [... > REFERENCE_TEMP]
     # ...
     return [],[],0,0
 
 
-def plot_results() -> None:
+def plot_results(
+    week_day_consumptions: pd.DataFrame,
+    x_series: np.array,
+    y_series: np.array,
+    reg_slope: float,
+    reg_intercept: float) -> None:
     """
         Task 5, plot the result
     """
@@ -56,6 +68,7 @@ def plot_results() -> None:
 
 facilities = [194, 209, 1066, 3971, 33606]
 consumption_df, temperature_df, facility_meter_df = data_reader.read_data()
+consumpions_combined = join_temperature(temperature_df, consumption_df, facility_meter_df)
+consumpions_ready = filter_to_weekdays(consumpions_combined)
 for facility_id in facilities:
     print(f'Analyzing {facility_id} now...')
-    
