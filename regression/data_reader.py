bd664182dc18cd1ec2c31300afb606636cdc2117
kalpanibhagya <kalpanibhagya.kb@gmail.com>
2022-12-02 08:51:13 +0200

    Add the code

--- regression/data_reader.py
+++ regression/data_reader.py
@@ -10,9 +10,9 @@ def read_data() -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
     Read csv, convert to local Europe/Helsinki format and return as dataframes
     """
     directory_path = os.path.join(os.getcwd(), 'regression', 'files')
-    consumption_df = pd.read_csv(os.path.join(directory_path, 'consumptions.csv')).set_index('meter_id')
-    temperature_df = pd.read_csv(os.path.join(directory_path, 'temperatures.csv')).set_index('facility_id')
-    facility_meter_df = pd.read_csv(os.path.join(directory_path, 'facilities.csv')).set_index('meter_id')
+    consumption_df = pd.read_csv(os.path.join(directory_path, 'consumptions.csv'))
+    temperature_df = pd.read_csv(os.path.join(directory_path, 'temperatures.csv'))
+    facility_meter_df = pd.read_csv(os.path.join(directory_path, 'facilities.csv'))
     consumption_df['timestamp'] = pd.to_datetime(consumption_df['timestamp'])
     consumption_df['timestamp'] = consumption_df['timestamp'].dt.tz_convert('Europe/Helsinki')
     temperature_df['timestamp'] = pd.to_datetime(temperature_df['timestamp'])
--- regression/regression_task.py
+++ regression/regression_task.py
@@ -9,7 +9,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 import data_reader
-
+from pandas.tseries.offsets import BDay #to find business days
 
 ELECTRICITY_QUANTITY_ID = 1
 REFERENCE_TEMP = 10
@@ -22,7 +22,28 @@ def join_temperature(temperatures: pd.DataFrame, consumptions: pd.DataFrame, fac
         facility_meter_df contains mapping between facility and meter
     """
     # ...
-    return consumptions
+    fac_id = []
+    print(consumptions['meter_id'])
+    for m_id in consumptions['meter_id'].values.tolist():
+        for fm in facilities_meters_list:
+            
+            if m_id in fm:
+                fac_id.append(facilities[facilities_meters_list.index(fm)])
+    
+    consumptions['facility_id'] = fac_id
+    #print(consumptions)
+
+    consumptions_new = temperatures.merge(consumptions,how="right", on=['facility_id','timestamp'])
+    
+    #mean imputation for missing value removal
+    from sklearn.impute import SimpleImputer
+
+    mean_imputer = SimpleImputer(strategy='mean')
+    consumptions_new['temperature'] = mean_imputer.fit_transform(consumptions_new['temperature'].values.reshape(-1,1))
+    consumptions_new['value'] = mean_imputer.fit_transform(consumptions_new['value'].values.reshape(-1,1))
+   
+
+    return consumptions_new
 
 
 def filter_to_weekdays(consumptions: pd.DataFrame) -> pd.DataFrame:
@@ -30,16 +51,32 @@ def filter_to_weekdays(consumptions: pd.DataFrame) -> pd.DataFrame:
         Task 2, filter out saturdays and sundays from the consumption data
     """
     # ...
+    isBusinessDay = BDay().onOffset
+    BusinessDay = consumptions["timestamp"].map(isBusinessDay)
+    consumptions["BusinessDay"] =  BusinessDay
+    
+    #print(len(consumptions))
+    consumptions = consumptions[consumptions.BusinessDay == True]
+    #print(len(consumptions))
+
     return consumptions
 
 
-def add_watts_per_square(facility_consumptions: pd.DataFrame, facility_area: float) -> None:
+def add_watts_per_square(facility_consumptions: list, facility_area: list) -> list:
     """
         Task 3, create new "column" which is W/m2
         Hint: formula is 'consumption * 1000 / facility_area'
     """
     # ...
-    return
+    watts_per_square = []
+    
+    for item in range(0,len(facility_consumptions)):
+        #print(facility_consumption[item])
+        result = facility_consumptions[item] * 1000 / facility_area[item]
+        #print(result)
+        watts_per_square.append(result)
+
+    return watts_per_square
 
 
 def linear_regression(facility_consumptions: pd.DataFrame) -> Tuple[np.array, np.array, float, float]:
@@ -50,7 +87,17 @@ def linear_regression(facility_consumptions: pd.DataFrame) -> Tuple[np.array, np
     """
     # Hint: cooling_consumptions = [... > REFERENCE_TEMP]
     # ...
-    return [],[],0,0
+    #print(type(facility_consumptions['temperature']))
+    cooling_consumptions = facility_consumptions[(facility_consumptions['temperature'] > REFERENCE_TEMP) & (facility_consumptions['temperature'] < MAX_COOLING_TEMP)]
+    X = cooling_consumptions['temperature'].to_numpy().reshape((-1, 1))
+    #print(X)
+    y = cooling_consumptions['W/m2'].to_numpy()
+    reg = LinearRegression().fit(X, y)
+    slope = reg.coef_
+
+    intercept = reg.intercept_
+
+    return X,y,slope,intercept
 
 
 def plot_results(
@@ -63,12 +110,55 @@ def plot_results(
         Task 5, plot the result
     """
     # ...
+    plt.scatter(x_series,y_series)
+    y_reg = reg_intercept + reg_slope * x_series
+    plt.plot(x_series, y_reg, 'r')
+    plt.show()
+
     return
 
 
 facilities = [194, 209, 1066, 3971, 33606]
 consumption_df, temperature_df, facility_meter_df = data_reader.read_data()
+consumption_df = consumption_df.sort_values(by=['meter_id']) 
+temperature_df = temperature_df.sort_values(by=['facility_id']) 
+facility_meter_df = facility_meter_df.sort_values(by=['facility_id']) 
+
+facilities_meters_list = []
+
+for i in facilities:
+    meter_list = []
+    for j in facility_meter_df.loc[facility_meter_df['facility_id'] == i, 'meter_id']:
+        meter_list.append(j)
+    facilities_meters_list.append(meter_list)
+
+
 consumpions_combined = join_temperature(temperature_df, consumption_df, facility_meter_df)
+
 consumpions_ready = filter_to_weekdays(consumpions_combined)
+
+
+values = consumpions_ready['value'].values.tolist()
+areas_df = facility_meter_df[['facility_id','area']] 
+areas_df = areas_df.drop_duplicates()
+
+#print(areas_df['area'])
+list1 = areas_df['area'].values.tolist()
+#print(list1)
+area_list = []
+
+
+for m in consumpions_ready['facility_id'].values.tolist():
+    #print(facilities.index(m))
+    area_list.append(list1[facilities.index(m)])
+
+# df1['area'] = area_list
+
+consumpions_ready['W/m2'] = add_watts_per_square(values, area_list) 
+
+x,y,slope,intercept = linear_regression(consumpions_ready)
+
 for facility_id in facilities:
     print(f'Analyzing {facility_id} now...')
+
+plot_results(consumpions_ready, x, y, slope,intercept)
